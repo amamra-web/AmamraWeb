89c078dc341a243f0338968711dc0a07526345a3
unknown <jasonyeh@cpp.edu>
2020-05-02 21:58:46 -0700

    Added predictive socket event handlers to socketindex.js

--- public/js/socketindex.js
+++ public/js/socketindex.js
@@ -46,30 +46,6 @@ Student.update = function(){
     }
     return pack;
 }
-///User Management System
-var USERS = {
-    //username:password hardcoded
-    "admin":"root",
-    "stu1":"anna",
-    "stu2":"bob",  
-}
- 
-var isValidPassword = function(data,cb){
-    setTimeout(function(){
-        cb(USERS[data.username] === data.password);
-    },10);
-}
-var isUsernameTaken = function(data,cb){
-    setTimeout(function(){
-        cb(USERS[data.username]);
-    },10);
-}
-var addUser = function(data,cb){
-    setTimeout(function(){
-        USERS[data.username] = data.password;
-        cb();
-    },10);
-}
 
 module.exports = {
     startCommunication : function(io) {
@@ -80,34 +56,44 @@ module.exports = {
             socket.id = Math.random();
             SOCKET_LIST[socket.id] = socket;
             
-            socket.on('signIn',function(data){
-                isValidPassword(data,function(res){
-                    if(res){
-                        Student.onConnect(socket);
-                        socket.emit('signInResponse',{success:true});
-                    } else {
-                        socket.emit('signInResponse',{success:false});         
-                    }
-                });
-            });
-            socket.on('signUp',function(data){
-                isUsernameTaken(data,function(res){
-                    if(res){
-                        socket.emit('signUpResponse',{success:false});     
-                    } else {
-                        addUser(data,function(){
-                            socket.emit('signUpResponse',{success:true});                  
-                        });
-                    }
-                });    
-            });
+            // socket.on('signIn',function(data){
+            //     isValidPassword(data,function(res){
+            //         if(res){
+            //             Student.onConnect(socket);
+            //             socket.emit('signInResponse',{success:true});
+            //         } else {
+            //             socket.emit('signInResponse',{success:false});         
+            //         }
+            //     });
+            // });
+            // socket.on('signUp',function(data){
+            //     isUsernameTaken(data,function(res){
+            //         if(res){
+            //             socket.emit('signUpResponse',{success:false});     
+            //         } else {
+            //             addUser(data,function(){
+            //                 socket.emit('signUpResponse',{success:true});                  
+            //             });
+            //         }
+            //     });    
+            // });
 
-            socket.on('hello', function(data){
-                console.log("hello is hit");
-                socket.broadcast.emit('helloBack', "hello");
+            // socket.on('hello', function(data){
+            //     console.log("hello is hit");
+            //     socket.broadcast.emit('helloBack', "hello");
+            // });
+
+            //Triggers when a user joins a quiz
+            socket.on('enable-quiz-up', function(data) {
+                //creates the socket channel
+                if(data.pin) {
+                    console.log(`Room ${data.pin} has been created, and host joined`);
+                    socket.join(data.pin)
+                } else {
+                    console.log("Room PIN is undefined");
+                }
             })
 
-            //Triggers when a user joins a room
             socket.on('user-join-up', function(data) {
                 //Emit down event to room  
                 console.log(`User has joined room ${data.pin}`); 
@@ -118,18 +104,34 @@ module.exports = {
                 socket.join(data.pin);         
             });
 
-            socket.on('userAnswerUp', function(data) {
-                if(data) {
-                    alert('user has answered');
-                } else {
-                    alert('something went wrong');
-                }
-                socket.emit('userAnswerDown')
+            //Handles the user requesting the current question
+            socket.on('user-request-q-up', function(data){
+                console.log('user-request-queue-up');
+                console.log(data.pin);
+
+                //Emit to host the question request
+                comms.in(`${data.pin}`).emit('user-request-q-down', data);
+            })
+
+            //Handles the host response going to server
+            socket.on('host-response-q-up', function(data) {
+                console.log('host-response-q-up');
+                
+                //emit to student the current question
+                comms.in(`${data.pin}`).emit('host-response-q-down');
+            })
+
+            //Handles the user submitting an answer
+            socket.on('user-answer-up', function(data) {
+                console.log('user-answer-up');
+
+                //emit to host about the answer
+                comms.in(`${data.pin}`).emit('user-answer-down');
             });
 
-            socket.on('nextQuestionUp', function(data) {
-                socket.in(data.pin).emit('nextQuestionDown');
-            });
+            // socket.on('host-next-question', function(data) {
+            //     socket.in(data.pin).emit('nextQuestionDown');
+            // });
            
             socket.on('disconnect',function(){
                 delete SOCKET_LIST[socket.id];
